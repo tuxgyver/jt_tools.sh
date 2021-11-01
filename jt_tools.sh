5a2f698bdbcc2e6fb12434c903dc14ba775d0dd5
fjohnny555 <johnny@free.fr>
2021-11-01 08:36:33 +0100

    Update jt_tools.sh

--- jt_tools.sh
+++ jt_tools.sh
@@ -1,7 +1,7 @@
 !#/bin/sh
 echo "Mise à jour..."
-apt update && apt -y full-upgrade > /dev/null
+apt update && apt full-upgrade -y > /dev/null
 echo "Installation des Outils..."
-apt install dnsutils cifs-utils nmon python3 htop sl inxi neofetch build-essentials gcc git curl net-tools -y
+apt install dnsutils cifs-utils nmon python3 figlet htop sl inxi neofetch build-essentials gcc git curl net-tools -y
 echo "Installation de bpytop"
 apt install -y bpytop
